d0a37506da7fa8310d8910e9582a0b5132b8b707
voinovd <voinovd96@gmail.com>
2017-11-07 15:42:46 +0300

    second commit

---
+++ Homework1/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import static java.lang.System.*;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int e=sc.nextInt();
+        sc.close();
+        int s;
+        s=a+b+c+d+e;
+        System.out.println(s);
+
+    }
+
+}
